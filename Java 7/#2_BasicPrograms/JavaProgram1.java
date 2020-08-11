f829ea502ddedfb3880d5d1c1577844d5a90996f
shrenik77130 <shrenik77130@gmail.com>
2020-08-11 14:18:52 +0530

    First Java Program

---
+++ Java 7/#2_BasicPrograms/JavaProgram1.java
@@ -0,0 +1,50 @@
+/* WAP to print Greeting Message using Java Code */
+
+
+class JavaProgram1
+{
+	public static void main(String []args)
+	{
+		System.out.print("\n Hello World");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+/*
+	Note:
+	-> static is keyword in OOP langauges
+	-> Any function if it is static then using class name you can call function
+
+		e.g.
+
+			class Person
+			{
+				public static void showPersonAge()
+				{
+					// age = 29
+				}
+			}
+
+			// call function
+			Person.showPersonAge();
+
+	-----------------------------------------------------------------
+
+	java/....
+
+		lang/...
+			System.java
+
+
+		io/....
+		PrintWriter.java
+
+*/
